0e4a382b37b5eced1ab35481c77dc4f69bde1fa8
Aman Khan <aman0786khan@gmail.com>
2020-09-19 18:45:11 +0530

    Product By Id middleware

--- routes/product.js
+++ routes/product.js
@@ -1,15 +1,17 @@
 const express = require("express");
-const { create } = require("../controllers/product");
+const { create, productById, read } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 const router = express.Router();
 
+router.get("/product/:productId", read);
 router.post(
-  "/product/create/:userId",
+  "/product/create/:userId/",
   [requireSignin, isAuth, isAdmin],
   create
 );
 
 router.param("userId", userById);
+router.param("productId", productById);
 
 module.exports = router;
