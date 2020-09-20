0826ada841c22c1b8ab7a89f995e7e9c585f8f81
Aman Khan <aman0786khan@gmail.com>
2020-09-20 06:31:23 +0530

    Filter products by sell/arrival

--- routes/product.js
+++ routes/product.js
@@ -5,27 +5,33 @@ const {
   read,
   remove,
   update,
+  list,
 } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 const router = express.Router();
 
 router.get("/product/:productId", read);
+
 router.post(
   "/product/create/:userId/",
   [requireSignin, isAuth, isAdmin],
   create
 );
+
 router.delete(
   "/product/:productId/:userId",
   [requireSignin, isAuth, isAdmin],
   remove
 );
+
 router.put(
   "/product/:productId/:userId",
   [requireSignin, isAuth, isAdmin],
   update
 );
+
+router.get("/products", list);
 
 router.param("userId", userById);
 router.param("productId", productById);
