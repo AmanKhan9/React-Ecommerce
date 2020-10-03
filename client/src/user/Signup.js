2cee9018ea75ccde21af5971281c308264efcd18
Aman Khan <aman0786khan@gmail.com>
2020-10-04 03:10:38 +0530

    Signup form binding

--- client/src/user/Signup.js
+++ client/src/user/Signup.js
@@ -1,11 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../core/Layout";
 
-const Signup = () => (
-  <Layout
-    title='Signup'
-    description='Signup to E-commerce application'
-  ></Layout>
-);
+const Signup = () => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    success: false,
+  });
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const signUpForm = () => (
+    <form>
+      <div className='form-group'>
+        <label className='text-muted'>Name</label>
+        <input
+          onChange={handleChange("name")}
+          type='text'
+          className='form-control'
+        />
+      </div>
+      <div className='form-group'>
+        <label className='text-muted'>Email</label>
+        <input
+          onChange={handleChange("email")}
+          type='email'
+          className='form-control'
+        />
+      </div>
+      <div className='form-group'>
+        <label className='text-muted'>Password</label>
+        <input
+          onChange={handleChange("password")}
+          type='password'
+          className='form-control'
+        />
+      </div>
+      <button className='btn btn-primary'>Submit</button>
+    </form>
+  );
+  return (
+    <Layout
+      className='container col-md-8 offset-md-2'
+      title='Signup'
+      description='Signup to E-commerce application'
+    >
+      {signUpForm()}
+      {JSON.stringify(values)}
+    </Layout>
+  );
+};
 
 export default Signup;
